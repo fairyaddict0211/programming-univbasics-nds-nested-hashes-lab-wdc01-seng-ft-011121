e0f6d683c1af1305d5c79cc808752b8fe034627e
Nyasha Perry <nyashaperry2005@yahoo.com>
2021-01-01 23:22:26 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -7,9 +7,9 @@ KATE_G = { name:  "Kate Gompert", occupation: "None" }
 BRUCE_G = { name:  "Bruce Green", occupation: "Fan of Mildred" }
 
 def assembled_aoh
-  results = [
-    {DON_G{ name:  "Don Gately", occupation: "Live-in Staff" } }, {JOELLE_VD { name:  "Joelle van Dyne", occupation: "Radio Personality"}, {PAT_M}, {"KATE_G"}, {"BRUCE_G"}
-  ]
+ [
+    DON_G, JOELLE_VD, PAT_M, KATE_G, BRUCE_G
+    ]
 end
 
 def literal_aoh
